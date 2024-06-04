6150d767c84e89e66ad6e542fcf456d0a7d5185b
jlqty <172397380+jltqy@users.noreply.github.com>
2024-06-04 16:16:12 +0100

    Progress II

--- src/StakingV2.sol
+++ src/StakingV2.sol
@@ -1,65 +1,84 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {IERC20} from "./../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+
 import "./ILQTYStaking.sol";
 
 contract UserProxy {
+    IERC20 public immutable lqty;
+    IERC20 public immutable lusd;
+
     ILQTYStaking public immutable stakingV1;
     address public immutable stakingV2;
+
     address public immutable user;
 
-    constructor(address stakingV1_, address user_) {
+    constructor(address lqty_, address lusd_, address stakingV1_, address user_) {
+        lqty = IERC20(lqty_);
+        lusd = IERC20(lusd_);
         stakingV1 = ILQTYStaking(stakingV1_);
         stakingV2 = msg.sender;
         user = user_;
     }
+
+    receive() external payable {}
 
     modifier onlyStakingV2() {
         require(msg.sender == stakingV2, "UserProxy: caller-not-stakingV2");
         _;
     }
 
-    modifier onlyUser() {
-        require(msg.sender == user, "UserProxy: caller-not-user");
-        _;
-    }
-
     function stake(uint256 amount) public onlyStakingV2 {
+        lqty.transferFrom(user, address(this), amount);
         stakingV1.stake(amount);
     }
 
     function unstake(uint256 amount) public onlyStakingV2 {
         stakingV1.unstake(amount);
-    }
 
-    function claimRewards() public onlyStakingV2() {
-        // stakingV1.claimRewards();
+        uint256 lusdBalance = lusd.balanceOf(address(this));
+        if (lusdBalance > 0) lusd.transfer(user, lusdBalance);
+        uint256 lqtyBalance = lqty.balanceOf(address(this));
+        if (lqtyBalance > 0) lqty.transfer(user, lqtyBalance);
+        uint256 ethBalance = address(this).balance;
+        if (ethBalance > 0) payable(user).transfer(ethBalance);
     }
 }
 
 contract StakingV2 {
+    address public immutable lqty;
+    address public immutable lusd;
     address public immutable stakingV1;
 
     mapping(address => address) public userProxies;
 
-    constructor(address stakingV1_) {
+    constructor(address lqty_, address lusd_, address stakingV1_) {
+        lqty = lqty_;
+        lusd = lusd_;
         stakingV1 = stakingV1_;
+    }
+
+    function deployUserProxy() public returns (address) {
+        require(userProxies[msg.sender] == address(0), "StakingV2: proxy-exists");
+        address userProxy = address(new UserProxy(lqty, lusd, stakingV1, msg.sender));
+
+        userProxies[msg.sender] = address(userProxy);
+
+        return userProxy;
     }
 
     function depositLQTY(uint256 amount) public {
         address userProxy = userProxies[msg.sender];
-        if (userProxy == address(0)) {
-            userProxy = address(new UserProxy(stakingV1, msg.sender));
-            userProxies[msg.sender] = address(userProxy);
-        }
+        require(userProxy != address(0), "StakingV2: unknown-user");
 
-        UserProxy(userProxy).stake(amount);
+        UserProxy(payable(userProxy)).stake(amount);
     }
 
     function withdrawLQTY(uint256 amount) public {
         address userProxy = userProxies[msg.sender];
         require(userProxy != address(0), "StakingV2: unknown-user");
 
-        UserProxy(userProxy).unstake(amount);
+        UserProxy(payable(userProxy)).unstake(amount);
     }
 }
--- test/StakingV2.t.sol
+++ test/StakingV2.t.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
+import {IERC20} from "./../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+
 import {StakingV2} from "../src/StakingV2.sol";
 
 
 
 contract StakingV2Test is Test {
     IERC20 constant private lqty = IERC20(address(0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D));
+    IERC20 constant private lusd = IERC20(address(0x5f98805A4E8be255a32880FDeC7F6728C6568bA0));
     address constant private stakingV1 = address(0x4f9Fbb3f1E99B56e0Fe2892e623Ed36A76Fc605d);
     address constant private user = address(0x64690353808dBcC843F95e30E071a0Ae6339EE1b);
 
@@ -15,13 +18,20 @@ contract StakingV2Test is Test {
 
     function setUp() public {
         vm.createSelectFork(vm.rpcUrl("mainnet"));
-        stakingV2 = new StakingV2(stakingV1);
+        stakingV2 = new StakingV2(address(lqty), address(lusd), stakingV1);
     }
 
     function test_depositLQTY() public {
         vm.startPrank(user);
-        lqty.approve(address(stakingV2), 1);
+
+        address userProxy = stakingV2.deployUserProxy();
+        lqty.approve(address(userProxy), 1);
         stakingV2.depositLQTY(1);
+
+        vm.warp(86400 * 30);
+
+        stakingV2.withdrawLQTY(1);
+
         vm.stopPrank();
     }
 }
