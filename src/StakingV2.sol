c5f56d5eb5ca95de419d16d1311b317d126449b7
jlqty <172397380+jltqy@users.noreply.github.com>
2024-06-05 14:59:40 +0100

    Progress V

--- src/StakingV2.sol
+++ src/StakingV2.sol
@@ -3,17 +3,59 @@ pragma solidity ^0.8.13;
 
 import {UserProxy} from "./UserProxy.sol";
 import {UserProxyFactory} from "./UserProxyFactory.sol";
+import {ILQTYStaking} from "./ILQTYStaking.sol";
+
+uint256 constant WAD = 1e18;
+uint256 constant ONE_YEAR = 31_536_000;
 
 contract StakingV2 is UserProxyFactory {
-    constructor(address lqty_, address lusd_, address stakingV1_) UserProxyFactory(lqty_, lusd_, stakingV1_) {}
+    uint256 public immutable deploymentTimestamp;
 
-    function depositLQTY(uint256 amount) public {
-        address userProxy = deriveUserProxyAddress(msg.sender);
-        UserProxy(payable(userProxy)).stake(msg.sender, amount);
+    uint256 public totalShares;
+    mapping(address => uint256) public sharesByUser;
+    mapping(address => uint256) public lqtyByUser;
+
+    constructor(address lqty_, address lusd_, address stakingV1_) UserProxyFactory(lqty_, lusd_, stakingV1_) {
+        deploymentTimestamp = block.timestamp;
     }
 
-    function withdrawLQTY(uint256 amount) public {
-        address userProxy = deriveUserProxyAddress(msg.sender);
-        UserProxy(payable(userProxy)).unstake(msg.sender, amount);
+    function currentShareRate() public view returns (uint256) {
+        // share exchange rate increases at a simple rate of 1000% per year since deployment, it is quoted on a 10^12 basis
+        return ((block.timestamp - deploymentTimestamp) * WAD / ONE_YEAR) + WAD;
+    }
+
+    // Voting power statically increases over time starting from 0 at time of share issuance
+    function votingPower(address user) public view returns (uint256) {
+        uint256 shares = sharesByUser[user];
+        uint256 weightedShares = shares * currentShareRate();
+        return (weightedShares - (shares * WAD)) / WAD;
+    }
+
+    function depositLQTY(uint256 lqtyAmount) public returns (uint256) {
+        UserProxy userProxy = UserProxy(payable(deriveUserProxyAddress(msg.sender)));
+        userProxy.stake(msg.sender, lqtyAmount);
+
+        uint256 shareAmount = lqtyAmount * WAD / currentShareRate();
+        sharesByUser[msg.sender] += shareAmount;
+
+        return shareAmount;
+    }
+
+    function withdrawShares(uint256 shareAmount) public returns (uint256) {
+        UserProxy userProxy = UserProxy(payable(deriveUserProxyAddress(msg.sender)));
+        uint256 totalLqty = ILQTYStaking(userProxy.stakingV1()).stakes(address(userProxy));
+
+        uint256 shares = sharesByUser[msg.sender];
+        uint256 lqtyAmount = (totalLqty * shareAmount) / shares;
+        userProxy.unstake(msg.sender, lqtyAmount);
+
+        sharesByUser[msg.sender] = shares - shareAmount;
+
+        return lqtyAmount;
+    }
+
+    // Claim staking rewards from StakingV1 without unstaking
+    function claimFromStakingV1() public {
+        UserProxy(payable(deriveUserProxyAddress(msg.sender))).unstake(msg.sender, 0);
     }
 }
--- test/StakingV2.t.sol
+++ test/StakingV2.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Test} from "forge-std/Test.sol";
 import {IERC20} from "./../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
 
-import {StakingV2} from "../src/StakingV2.sol";
+import {StakingV2, WAD} from "../src/StakingV2.sol";
 
 contract StakingV2Test is Test {
     IERC20 private constant lqty = IERC20(address(0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D));
@@ -19,23 +19,42 @@ contract StakingV2Test is Test {
         stakingV2 = new StakingV2(address(lqty), address(lusd), stakingV1);
     }
 
-    function test_deployUserProxy_depositLQTY_withdrawLQTY() public {
-        vm.startPrank(user);
-        address userProxy = stakingV2.deployUserProxy();
-        vm.stopPrank();
+    function test_deployUserProxy() public {
+        address userProxy = stakingV2.deriveUserProxyAddress(user);
 
-        vm.startPrank(address(this));
+        vm.startPrank(user);
+        assertEq(stakingV2.deployUserProxy(), userProxy);
+        vm.expectRevert();
         stakingV2.deployUserProxy();
         vm.stopPrank();
 
+        stakingV2.deployUserProxy();
+        assertEq(stakingV2.deriveUserProxyAddress(user), userProxy);
+    }
+
+    function test_depositLQTY_withdrawShares() public {
         vm.startPrank(user);
+        
+        // deploy
+        address userProxy = stakingV2.deployUserProxy();
 
-        lqty.approve(address(userProxy), 1);
-        stakingV2.depositLQTY(1);
+        // deposit 1 LQTY
+        lqty.approve(address(userProxy), 1e18);
+        assertEq(stakingV2.depositLQTY(1e18), 1e18);
+        assertEq(stakingV2.sharesByUser(user), 1e18);
 
-        vm.warp(86400 * 30);
+        // deposit 2 LQTY
+        vm.warp(block.timestamp + 86400 * 30);
+        lqty.approve(address(userProxy), 2e18);
+        assertEq(stakingV2.depositLQTY(2e18), 2e18 * WAD / stakingV2.currentShareRate());
+        assertEq(stakingV2.sharesByUser(user), 1e18 + 2e18 * WAD / stakingV2.currentShareRate()); 
 
-        stakingV2.withdrawLQTY(1);
+        // withdraw 0.5 half of shares
+        vm.warp(block.timestamp + 86400 * 30);
+        assertEq(stakingV2.withdrawShares(stakingV2.sharesByUser(user) / 2), 1.5e18);
+
+        // withdraw remaining shares
+        assertEq(stakingV2.withdrawShares(stakingV2.sharesByUser(user)), 1.5e18);
 
         vm.stopPrank();
     }
