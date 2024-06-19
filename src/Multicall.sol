041fd23e3bf3855c41b83848a2e957c14fb84a45
jlqty <172397380+jltqy@users.noreply.github.com>
2024-06-19 14:57:47 +0100

    Fmt

--- src/Multicall.sol
+++ src/Multicall.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-
 /// @title Multicall interface
 /// @notice Enables calling multiple methods in a single call to the contract
 interface IMulticall {
@@ -16,7 +15,7 @@ interface IMulticall {
 /// @notice Enables calling multiple methods in a single call to the contract
 abstract contract Multicall is IMulticall {
     /// @inheritdoc IMulticall
-    function multicall(bytes[] calldata data) public override payable returns (bytes[] memory results) {
+    function multicall(bytes[] calldata data) public payable override returns (bytes[] memory results) {
         results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
             (bool success, bytes memory result) = address(this).delegatecall(data[i]);
