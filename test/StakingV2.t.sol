342d174181de9fbf8368285a022f59d96c40dd35
jlqty <172397380+jltqy@users.noreply.github.com>
2024-06-19 10:34:41 +0200

    updating user wallet to Binance to ensure that we have LQTY balance

--- test/StakingV2.t.sol
+++ test/StakingV2.t.sol
@@ -17,7 +17,7 @@ contract StakingV2Test is Test {
     IERC20 private constant lqty = IERC20(address(0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D));
     IERC20 private constant lusd = IERC20(address(0x5f98805A4E8be255a32880FDeC7F6728C6568bA0));
     address private constant stakingV1 = address(0x4f9Fbb3f1E99B56e0Fe2892e623Ed36A76Fc605d);
-    address private constant user = address(0x64690353808dBcC843F95e30E071a0Ae6339EE1b);
+    address private constant user = address(0xF977814e90dA44bFA03b6295A0616a897441aceC); // Binance wallet
 
     uint256 private constant MIN_CLAIM = 500e18;
     uint256 private constant MIN_ACCRUAL = 1000e18;
