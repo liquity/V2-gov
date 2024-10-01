5c40482b2108a2b3bbaffb1d76ebef4eb284880a
jlqty <172397380+jltqy@users.noreply.github.com>
2024-10-01 15:13:56 +0100

    Add deployment script for UniV4Donations

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 
 import {IERC20} from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
 
-// import {IPoolManager, PoolManager, Deployers, TickMath, Hooks} from "v4-core/test/utils/Deployers.sol";
-// import {PoolModifyLiquidityTest} from "v4-core/src/test/PoolModifyLiquidityTest.sol";
+import {PoolManager, Deployers, Hooks} from "v4-core/test/utils/Deployers.sol";
+import {PoolModifyLiquidityTest} from "v4-core/src/test/PoolModifyLiquidityTest.sol";
 import {ICurveStableswapFactoryNG} from "../src/interfaces/ICurveStableswapFactoryNG.sol";
 import {ICurveStableswapNG} from "../src/interfaces/ICurveStableswapNG.sol";
 import {ILiquidityGauge} from "./../src/interfaces/ILiquidityGauge.sol";
@@ -16,8 +16,11 @@ import {IGovernance} from "../src/interfaces/IGovernance.sol";
 import {Governance} from "../src/Governance.sol";
 import {UniV4Donations} from "../src/UniV4Donations.sol";
 import {CurveV2GaugeRewards} from "../src/CurveV2GaugeRewards.sol";
+import {BaseHook, Hooks} from "../src/utils/BaseHook.sol";
 
-contract DeploymentScript is Script {
+import {UniV4DonationsImpl} from "../test/UniV4Donations.t.sol";
+
+contract DeploymentScript is Script, Deployers {
     // Environment Constants
     IERC20 private constant lqty = IERC20(address(0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D));
     IERC20 private constant bold = IERC20(address(0x5f98805A4E8be255a32880FDeC7F6728C6568bA0));
@@ -41,19 +44,20 @@ contract DeploymentScript is Script {
     // UniV4Donations Constants
     uint256 private immutable VESTING_EPOCH_START = block.timestamp;
     uint256 private constant VESTING_EPOCH_DURATION = 7 days;
-    // address private constant POOL_MANAGER = address(0x0000000000000000000000000000000000000000);
     address private constant TOKEN = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     uint24 private constant FEE = 0;
     int24 private constant TICK_SPACING = 0;
+    int24 constant MAX_TICK_SPACING = 32767;
 
     // CurveV2GaugeRewards Constants
-    // address private constant GAUGE = address(0x0000000000000000000000000000000000000000);
     uint256 private constant DURATION = 7 days;
 
     Governance private governance;
     address[] private initialInitiatives;
 
-    UniV4Donations private uniV4Donations;
+    UniV4Donations private uniV4Donations =
+        UniV4Donations(address(uint160(Hooks.AFTER_INITIALIZE_FLAG | Hooks.AFTER_ADD_LIQUIDITY_FLAG)));
+
     CurveV2GaugeRewards private curveV2GaugeRewards;
 
     ICurveStableswapNG private curvePool;
@@ -84,24 +88,35 @@ contract DeploymentScript is Script {
         );
     }
 
-    // function deployUniV4Donations(uint256 _nonce) private {
-    //     manager = new PoolManager(500000);
-    //     modifyLiquidityRouter = new PoolModifyLiquidityTest(manager);
+    function deployUniV4Donations(uint256 _nonce) private {
+        manager = new PoolManager(500000);
+        modifyLiquidityRouter = new PoolModifyLiquidityTest(manager);
 
-    //     uniV4Donations = new UniV4Donations(
-    //         address(vm.computeCreateAddress(address(this), _nonce)),
-    //         address(bold),
-    //         address(lqty),
-    //         VESTING_EPOCH_START,
-    //         VESTING_EPOCH_DURATION,
-    //         address(manager),
-    //         address(usdc),
-    //         FEE,
-    //         TICK_SPACING
-    //     );
+        UniV4DonationsImpl impl = new UniV4DonationsImpl(
+            address(vm.computeCreateAddress(address(this), _nonce)),
+            address(bold),
+            address(lqty),
+            block.timestamp,
+            EPOCH_DURATION,
+            address(manager),
+            address(usdc),
+            400,
+            MAX_TICK_SPACING,
+            BaseHook(address(uniV4Donations))
+        );
 
-    //     initialInitiatives.push(address(uniV4Donations));
-    // }
+        (, bytes32[] memory writes) = vm.accesses(address(impl));
+        vm.etch(address(uniV4Donations), address(impl).code);
+        // for each storage key that was written during the hook implementation, copy the value over
+        unchecked {
+            for (uint256 i = 0; i < writes.length; i++) {
+                bytes32 slot = writes[i];
+                vm.store(address(uniV4Donations), slot, vm.load(address(impl), slot));
+            }
+        }
+
+        initialInitiatives.push(address(uniV4Donations));
+    }
 
     function deployCurveV2GaugeRewards(uint256 _nonce) private {
         address[] memory _coins = new address[](2);
@@ -126,7 +141,11 @@ contract DeploymentScript is Script {
         gauge = ILiquidityGauge(curveFactory.deploy_gauge(address(curvePool)));
 
         curveV2GaugeRewards = new CurveV2GaugeRewards(
-            address(vm.computeCreateAddress(address(this), _nonce)), address(bold), address(lqty), address(gauge), DURATION
+            address(vm.computeCreateAddress(address(this), _nonce)),
+            address(bold),
+            address(lqty),
+            address(gauge),
+            DURATION
         );
 
         initialInitiatives.push(address(curveV2GaugeRewards));
@@ -135,7 +154,7 @@ contract DeploymentScript is Script {
     function run() public {
         // vm.broadcast();
 
-        // deployUniV4Donations(vm.getNonce(address(this)) + 2);
+        deployUniV4Donations(vm.getNonce(address(this)) + 2);
         deployCurveV2GaugeRewards(vm.getNonce(address(this)) + 1);
         deployGovernance();
 
