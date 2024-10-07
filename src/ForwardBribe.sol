fc5bff2bd1a650b6dcb68c537b40753d1a59d238
Colin Platt <colingplatt@gmail.com>
2024-10-07 17:42:14 +0200

    basic forwarder

---
+++ src/ForwardBribe.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {BribeInitiative} from "./BribeInitiative.sol";
+
+contract ForwardBribe is BribeInitiative {
+    address public immutable receiver;
+
+    constructor(address _governance, address _bold, address _bribeToken, address _receiver)
+        BribeInitiative(_governance, _bold, _bribeToken)
+    {
+        receiver = _receiver;
+    }
+
+    function forwardBribe() external {
+        governance.claimForInitiative(address(this));
+
+        uint boldAmount = bold.balanceOf(address(this));
+        uint bribeTokenAmount = bribeToken.balanceOf(address(this));
+
+        if (boldAmount != 0) bold.safeTransfer(receiver, boldAmount);
+        if (bribeTokenAmount != 0) bribeToken.safeTransfer(receiver, bribeTokenAmount);
+    }
+}
