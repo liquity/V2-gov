a932aa0c4ccddada7a1cfc2a1083914ebf53793e
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2024-10-14 08:15:16 -0300

    feat: fuzz tests for encoding + decoding allocation

---
+++ test/EncodingDecoding.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+library EncodingDecoding {
+    function encodeLQTYAllocation(uint88 _lqty, uint32 _averageTimestamp) public pure returns (uint224) {
+        uint224 _value = (uint224(_lqty) << 32) | _averageTimestamp;
+        return _value;
+    }
+
+    function decodeLQTYAllocation(uint224 _value) public pure returns (uint88, uint32) {
+        return (uint88(_value >> 32), uint32(_value));
+    }
+}
+
+contract EncodingDecodingTest is Test {
+    // value -> encoding -> decoding -> value 
+    function test_encoding_and_decoding_symmetrical(uint88 lqty, uint32 averageTimestamp) public {
+        uint224 encodedValue = EncodingDecoding.encodeLQTYAllocation(lqty, averageTimestamp);
+        (uint88 decodedLqty, uint32 decodedAverageTimestamp) = EncodingDecoding.decodeLQTYAllocation(encodedValue);
+
+        assertEq(lqty, decodedLqty);
+        assertEq(averageTimestamp, decodedAverageTimestamp);
+    }
+
+    // receive -> undo -> check -> redo -> compare
+    function test_receive_undo_compare(uint224 encodedValue) public {
+        (uint88 decodedLqty, uint32 decodedAverageTimestamp) = EncodingDecoding.decodeLQTYAllocation(encodedValue);
+
+        uint224 encodedValue2 = EncodingDecoding.encodeLQTYAllocation(decodedLqty, decodedAverageTimestamp);
+        (uint88 decodedLqty2, uint32 decodedAverageTimestamp2) = EncodingDecoding.decodeLQTYAllocation(encodedValue);
+
+        assertEq(encodedValue, encodedValue2, "encoded values not equal");
+        assertEq(decodedLqty, decodedLqty2, "decoded lqty not equal");
+        assertEq(decodedAverageTimestamp, decodedAverageTimestamp2, "decoded timestamps not equal");
+    }
+}
