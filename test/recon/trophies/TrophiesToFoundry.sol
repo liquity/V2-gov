b6fc28a96f53b3b6be170d2d806b1e8011b7c4e9
gallo <alex@entreprenerd.xyz>
2024-10-16 16:42:07 +0200

    chore: further simplify trophy

--- test/recon/trophies/TrophiesToFoundry.sol
+++ test/recon/trophies/TrophiesToFoundry.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
 import {TargetFunctions} from "../TargetFunctions.sol";
+import {Governance} from "src/Governance.sol";
 import {FoundryAsserts} from "@chimera/FoundryAsserts.sol";
 
 import {console} from "forge-std/console.sol";
@@ -23,30 +24,32 @@ contract TrophiesToFoundry is Test, TargetFunctions, FoundryAsserts {
 // We can change the property to ignore DISABLED strategies
 // Or we would have to rethink the architecture 
 function test_property_sum_of_lqty_global_user_matches_0() public {
-  
+
    vm.roll(161622);
-   vm.warp(9999999999 + 1793404);
+   vm.warp(block.timestamp + 1793404);
    vm.prank(0x0000000000000000000000000000000000030000);
    property_sum_of_lqty_global_user_matches();
   
    vm.roll(273284);
-   vm.warp(9999999999 + 3144198);
+   vm.warp(block.timestamp + 3144198);
    vm.prank(0x0000000000000000000000000000000000020000);
    governance_depositLQTY(3501478328989062228745782);
   
    vm.roll(273987);
-   vm.warp(9999999999 + 3148293);
+   vm.warp(block.timestamp + 3148293);
    vm.prank(0x0000000000000000000000000000000000030000);
    governance_allocateLQTY_clamped_single_initiative(0, 5285836763643083359055120749, 0);
-  
-   vm.roll(303163);
-   vm.warp(9999999999 + 3234641);
-   vm.prank(0x0000000000000000000000000000000000030000);
+
+
    governance_unregisterInitiative(0);
-  
-   vm.roll(303170);
-   vm.warp(9999999999 + 3234929);
-   vm.prank(0x0000000000000000000000000000000000010000);
    property_sum_of_lqty_global_user_matches();
 }
+
+    function _getInitiativeStatus(address _initiative) internal returns (uint256) {
+        (Governance.InitiativeStatus status, , ) = governance.getInitiativeState(_getDeployedInitiative(0)); 
+        return uint256(status);
+    }
+
+
+
 }
