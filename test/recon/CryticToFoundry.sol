15411923eb6db75dfa280418ee3188c08f95ecf9
gallo <alex@entreprenerd.xyz>
2024-10-16 15:54:33 +0200

    feat: trophies

--- test/recon/CryticToFoundry.sol
+++ test/recon/CryticToFoundry.sol
@@ -12,39 +12,5 @@ import {console} from "forge-std/console.sol";
 contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
     function setUp() public {
         setup();
-    }
-		
-// forge test --match-test test_property_sum_of_lqty_global_user_matches_0 -vv 
- 
-function test_property_sum_of_lqty_global_user_matches_0() public {
-  
-   vm.roll(161622);
-   vm.warp(9999999999 + 1793404);
-   vm.prank(0x0000000000000000000000000000000000030000);
-   property_sum_of_lqty_global_user_matches();
-  
-   vm.roll(273284);
-   vm.warp(9999999999 + 3144198);
-   vm.prank(0x0000000000000000000000000000000000020000);
-   governance_depositLQTY(3501478328989062228745782);
-  
-   vm.roll(273987);
-   vm.warp(9999999999 + 3148293);
-   vm.prank(0x0000000000000000000000000000000000030000);
-   governance_allocateLQTY_clamped_single_initiative(0, 5285836763643083359055120749, 0);
-  
-   vm.roll(303163);
-   vm.warp(9999999999 + 3234641);
-   vm.prank(0x0000000000000000000000000000000000030000);
-   governance_unregisterInitiative(0);
-  
-   vm.roll(303170);
-   vm.warp(9999999999 + 3234929);
-   vm.prank(0x0000000000000000000000000000000000010000);
-   property_sum_of_lqty_global_user_matches();
-}
-		
-		
-		
-		
+    }		
 }
