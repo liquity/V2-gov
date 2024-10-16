568f189a41e2641d3ab35dc09cdcc482282006d4
gallo <alex@entreprenerd.xyz>
2024-10-16 14:08:00 +0200

    chore: sample broken pro

--- test/recon/CryticToFoundry.sol
+++ test/recon/CryticToFoundry.sol
@@ -13,39 +13,61 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
     function setUp() public {
         setup();
     }
+		
 
-// forge test --match-test test_property_sum_of_lqty_global_user_matches_0 -vv 
+// forge test --match-test test_property_sum_of_user_initiative_allocations_0 -vv 
  
-function test_property_sum_of_lqty_global_user_matches_0() public {
-   vm.roll(block.number + 54184);
-   vm.warp(block.timestamp + 65199);
+function test_property_sum_of_user_initiative_allocations_0() public {
+  
+   vm.roll(2);
+   vm.warp(86000000 + 2);
    vm.prank(0x0000000000000000000000000000000000010000);
-   governance_depositLQTY(10618051687797035123500145);
+   helper_deployInitiative();
   
-   vm.roll(block.number + 103930);
-   vm.warp(block.timestamp + 635494);
+   vm.roll(20338);
+   vm.warp(86000000 + 359683);
    vm.prank(0x0000000000000000000000000000000000030000);
-   governance_allocateLQTY_clamped_single_initiative(0, 0, 1231231);
-
-    (uint88 user_allocatedLQTY, ) = governance.userStates(user);
-
-    assertTrue(user_allocatedLQTY > 0, "Something is allocated");
-
-   // Allocates `10597250933489619569146227`
-    (
-        uint88 countedVoteLQTY, uint32 countedVoteLQTYAverageTimestamp 
-            // uint32 after_user_countedVoteLQTYAverageTimestamp // TODO: How do we do this?
-    ) = governance.globalState();
-    console.log("countedVoteLQTYAverageTimestamp", countedVoteLQTYAverageTimestamp);
-    assertTrue(countedVoteLQTY > 0, "Something is counted");
-
+   helper_deployInitiative();
   
-   vm.roll(block.number + 130098);
-   vm.warp(block.timestamp + 1006552);
+   vm.roll(35511);
+   vm.warp(86000000 + 718072);
+   vm.prank(0x0000000000000000000000000000000000030000);
+   helper_deployInitiative();
+  
+   vm.roll(94412);
+   vm.warp(86000000 + 999244);
+   vm.prank(0x0000000000000000000000000000000000010000);
+   helper_deployInitiative();
+  
+   vm.roll(161790);
+   vm.warp(86000000 + 2651694);
    vm.prank(0x0000000000000000000000000000000000020000);
-   property_sum_of_lqty_global_user_matches();
+   governance_depositLQTY(646169017059856542762865);
+  
+   vm.roll(186721);
+   vm.warp(86000000 + 2815428);
+   vm.prank(0x0000000000000000000000000000000000020000);
+   governance_registerInitiative(63);
+  
+   vm.roll(257296);
+   vm.warp(86000000 + 3261349);
+   vm.prank(0x0000000000000000000000000000000000020000);
+   helper_deployInitiative();
+  
+   vm.roll(333543);
+   vm.warp(86000000 + 4091708);
+   vm.prank(0x0000000000000000000000000000000000020000);
+   helper_deployInitiative();
+  
+   vm.roll(368758);
+   vm.warp(86000000 + 4314243);
+   vm.prank(0x0000000000000000000000000000000000020000);
+   governance_allocateLQTY_clamped_single_initiative(3, 29956350487679649024950075925, 0);
+  
+   vm.roll(375687);
+   vm.warp(86000000 + 4704876);
+   vm.prank(0x0000000000000000000000000000000000020000);
+   property_sum_of_user_initiative_allocations();
 }
-		
-		
 		
 }
