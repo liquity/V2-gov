408205094c3259ee59c563135f3ed29f5741a750
gallo <alex@entreprenerd.xyz>
2024-10-19 11:44:46 +0200

    chore: repros

--- test/recon/CryticToFoundry.sol
+++ test/recon/CryticToFoundry.sol
@@ -12,6 +12,8 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
         setup();
     }
 
+    /// Example fixed bribes properties
+    // Use `https://getrecon.xyz/tools/echidna` to scrape properties into this format
     // forge test --match-test test_property_BI03_1 -vv 
     function test_property_BI03_1() public {
         vm.roll(block.number + 1);
@@ -31,5 +33,23 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
         vm.warp(block.timestamp + 606998);
         governance_allocateLQTY_clamped_single_initiative(0,0,1);
         property_BI04();
+    }
+
+    // forge test --match-test test_property_BI11_3 -vv 
+    function test_property_BI11_3() public {
+
+        vm.roll(block.number + 1);
+        vm.warp(block.timestamp + 461046);
+        vm.prank(0x0000000000000000000000000000000000030000);
+        governance_depositLQTY(2);
+
+        vm.roll(block.number + 1);
+        vm.warp(block.timestamp + 301396);
+        governance_allocateLQTY_clamped_single_initiative(0,1,0);
+
+        vm.roll(block.number + 1);
+        vm.warp(block.timestamp + 450733);
+        initiative_claimBribes(0,3,0,0);
+        property_BI11();
     }
 }
