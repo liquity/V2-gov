7859f6295c0fc8ceb3db69e566eae07f52d2a9d0
gallo <alex@entreprenerd.xyz>
2024-10-28 20:09:32 +0100

    fix: use calldata to check dups

--- src/utils/UniqueArray.sol
+++ src/utils/UniqueArray.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 /// @dev Checks that there's no duplicate addresses
 /// @param arr - List to check for dups
-function _requireNoDuplicates(address[] memory arr) pure {
+function _requireNoDuplicates(address[] calldata arr) pure {
     uint256 arrLength = arr.length;
     // only up to len - 1 (no j to check if i == len - 1)
     for (uint i; i < arrLength - 1;) {
