4ded0c77c91bcee6a331d79724c82cf171f3564d
Entreprenerd <alex@entreprenerd.xyz>
2024-11-02 11:36:42 +0100

    feat: repro for claim bribe

--- test/recon/CryticToFoundry.sol
+++ test/recon/CryticToFoundry.sol
@@ -15,4 +15,29 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
         setup();
         
     }
+
+// forge test --match-test test_property_BI05_3 -vv 
+ function test_property_BI05_3() public {
+
+ initiative_depositBribe(107078662,31,2,0);
+
+     vm.warp(block.timestamp + 607467);
+
+     vm.roll(block.number + 1);
+
+ property_BI05();
+
+ }
+// forge test --match-test test_can_claim -vv 
+ function test_can_claim() public {
+     vm.warp(block.timestamp + governance.EPOCH_DURATION());
+    depositTsIsRational(1);
+    initiative_depositBribe(1,0,3,0);
+    governance_allocateLQTY_clamped_single_initiative(0, 1, 0);
+    vm.warp(block.timestamp + governance.EPOCH_DURATION()); // 4
+    assertEq(governance.epoch(), 4, "4th epoch");
+    initiative_claimBribes(3, 3, 3, 0);
+    canary_has_claimed();
+ }
+
 }
