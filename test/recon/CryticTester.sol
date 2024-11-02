d60587edcf239e811d0c2a4b795770cfac6138f5
Entreprenerd <alex@entreprenerd.xyz>
2024-11-02 11:36:51 +0100

    feat: extra config for running locally

--- test/recon/CryticTester.sol
+++ test/recon/CryticTester.sol
@@ -5,6 +5,7 @@ import {TargetFunctions} from "./TargetFunctions.sol";
 import {CryticAsserts} from "@chimera/CryticAsserts.sol";
 
 // echidna . --contract CryticTester --config echidna.yaml
+// echidna . --contract CryticTester --config echidna.yaml --format text --test-limit 1000000 --test-mode assertion
 // medusa fuzz
 contract CryticTester is TargetFunctions, CryticAsserts {
     constructor() payable {
