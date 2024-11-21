4941886cdd3988236b9feb5217ed551c7b0a13cd
Daniel Simon <daniel.attila.simon@gmail.com>
2024-11-21 12:19:47 +0700

    chore: fix typo in comment

--- src/Governance.sol
+++ src/Governance.sol
@@ -127,7 +127,7 @@ contract Governance is Multicall, UserProxyFactory, ReentrancyGuard, Ownable, IG
         for (uint256 i = 0; i < _initiatives.length; i++) {
             initiativeStates[_initiatives[i]] = InitiativeState(0, 0, 0, 0, 0);
 
-            // Register initial initiatives is the earliest possible epoch, which lets us make them votable immediately
+            // Register initial initiatives in the earliest possible epoch, which lets us make them votable immediately
             // post-deployment if we so choose, by backdating the first epoch at least EPOCH_DURATION in the past.
             registeredInitiatives[_initiatives[i]] = 1;
 
